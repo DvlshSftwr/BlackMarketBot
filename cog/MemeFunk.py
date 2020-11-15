b96c7a919f9f0628c905d683e7738ccfec1de3f9
Fluffykins Allmighty <71295247+DvlshSftwr@users.noreply.github.com>
2020-11-15 15:38:48 -0500

    Update MemeFunk.py

--- cog/MemeFunk.py
+++ cog/MemeFunk.py
@@ -1,7 +1,9 @@
 import discord
+import random
 
 from discord.ext import commands
-from Constants import * 
+from Constants import *
+from RandomNumberGenerator import RNG
 
 class MemeFunk(commands.Cog):
 	
@@ -35,43 +37,16 @@ class MemeFunk(commands.Cog):
 		""" 'Twinkie House' someone"""
 		
 		embed6 = discord.Embed(title='Twinkie House!', description='itty bitty baby...itty bitty boat...', color=0xACFA58)
-		rand_num = random.randint(0, 5)
+		rand_num = random.randint(1, 7)
 		habeeb_it = f"Habeeb it, {member.name}!"
 		i_dont = f'{member} : \"I don\'t believe it!\"'
-		id = str(member.id)
+		img = discord.File(f"img/twinkie-house{rand_num}.png", "image.png")
 		
-		
-		await ctx.send(f'<@{id}>')
-		
-		if rand_num == 5:
-			embed_5.set_image(url = Const.URL.TWINKIE_HOUSES[5])
-			embed_5.add_field(name=i_dont, value=habeeb_it, inline=False)
-		
-		elif rand_num == 4:
-			embed6.set_image(url = Const.URL.TWINKIE_HOUSES[4])
-			embed6.add_field(name=i_dont, value=habeeb_it, inline=False)
-		
-		elif rand_num == 3:
-			embed6.set_image(url = Const.URL.TWINKIE_HOUSES[3])
-			embed6.add_field(name=i_dont, value=habeeb_it, inline=False)
-		
-		elif rand_num == 2:
-			embed6.set_image(url= Const.URL.TWINKIE_HOUSES[2])
-			embed6.add_field(name=i_dont, value=habeeb_it, inline=False)
-		
-		elif rand_num == 1:
-			embed6.set_image(url = Const.URL.TWINKIE_HOUSES[1])
-			embed6.add_field(name=i_dont, value=habeeb_it, inline=False)
-		
-		elif rand_num == 0:
-			embed6.set_image(url = Const.URL.TWINKIE_HOUSES[0])
-			embed6.add_field(name=i_dont, value=habeeb_it, inline=False)
-		
+		embed6.set_image(url = 'attachment://image.png')
+		embed6.add_field(name=i_dont, value=habeeb_it, inline=False)
 		embed6.set_footer(text=str(reason))
 		
-		await set_bank_data(bank)
-		
-		await ctx.send(embed=embed6)
+		await ctx.send(file = img, embed = embed6)
 	
 	
 	@commands.command(name = Const.CMD.MTL_BENDING)
@@ -82,6 +57,32 @@ class MemeFunk(commands.Cog):
 		embed11.set_image(url = 'https://media.discordapp.net/attachments/709438676647673948/776701867161419786/124520260_10223642640195657_2541179891297595795_o.png?width=478&height=478')
 		
 		await ctx.send(embed = embed11)
+	
+	
+	@commands.command(name = 'amd-cpu-tutorial')
+	async def amd_cpu_tutorial(self, ctx : commands.Context):
+		"""how to modify an intel mobo to take an amd cpu"""
+		
+		stoopid_embed = discord.Embed(title = "HOW TO INSTALL AN AMD CPU INTO AN INTEL MOBO", description = "```css\nhow to modify an intel motherboard to take an amd cpu```", color = 0x9933FF)
+		
+		await ctx.send(embed = stoopid_embed)
+		await ctx.send('https://unixism.xyz/share/amd_guide.mp4')
+	
+	
+	@commands.command(name='rand=')
+	async def rand_hex(self, ctx, base = 'dec', str_len = 2):
+		"""generate random number strings"""
+		
+		result = RNG.generate(base, str_len)
+    
+		if base == 'hex':
+			await ctx.send(f'```cpp\nYour numerical string is :\n0x{result.upper()}```')
+	
+		else:
+			await ctx.send(f'```cpp\nYour numerical string is :\n{result}```')
+	
+	
+	
 
 
 def setup(client):
