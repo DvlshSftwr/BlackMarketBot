e945ec307f5cac3a81a09e5792f5baecdd0dd240
Fluffykins Allmighty <71295247+DvlshSftwr@users.noreply.github.com>
2020-11-14 19:14:02 -0500

    Add files via upload

---
+++ BlackMarket.py
@@ -0,0 +1,289 @@
+import Constants
+import discord
+import os
+import sys
+import subprocess
+import random
+
+from discord.ext import commands
+from Constants import *
+from cog.DankBank import DankBank
+from cog.Store import Store
+
+
+BlackMarket = commands.Bot(command_prefix = Const.PREFIX, description = Const.APP_DESC)
+
+
+@BlackMarket.event
+async def on_ready():
+	cmd = BlackMarket.command
+	name = BlackMarket.user
+	id = str(BlackMarket.user.id)
+	
+	print(Const.Login.LOGGED_IN.format(name, id))
+	print(Const.Login.LOADING.format(cmd))
+	
+	await BlackMarket.change_presence(status = discord.Status.online, activity = discord.Game(Const.GAME))
+	
+	print(Const.Login.OPEN.format(name.name))
+
+
+@BlackMarket.event
+async def on_message(message : discord.Message):
+		usr = message.author
+		bot_name = BlackMarket.user.name
+		if usr.bot:
+			print(f'{bot_name}    : Output Sent')
+			return
+			
+		else:
+			bank = await DankBank.get_bank_data()
+			member = message.author
+			msg_len = len(message.content)
+			rand_num = random.randint(1, 4)
+			munnies = msg_len * rand_num / 10
+			cap = 150
+			
+			while munnies > cap:
+				rand_num = random.randint(1, 5)
+				munnies = int(msg_len * rand_num / 10)
+			
+			print(f"\n\nUSER           : {usr}\nID             : {usr.id}\nMessage Length : {msg_len}\nPayout         : {munnies}")
+			
+			if not message.content.startswith(Const.PREFIX):
+				bank[str(member.id)]["bank"] += munnies
+				print(f'{bot_name}    : payout deposited into bank account of : {usr.name}')
+			
+			else:
+				print(f'{bot_name}    : "{message.content}" is a command, so payout not deposited')
+			
+			await DankBank.set_bank_data(bank)
+			await BlackMarket.process_commands(message)
+
+
+
+"""
+                  ==============================================================================================================
+                  ||     AA     PPPPPP  pppppp     CCCCCCC  OOOOOO  MM      MM MM      MM     AA     NN    NN DDDDDD   SSSSSS ||
+                  ||    AAAA    PP   PP pp   pp   CC       OO    OO MMMM  MMMM MMMM  MMMM    AAAA    NNNN  NN DD   DD SS      ||
+                  ||   AA  AA   PPPPPP  pppppp    CC       OO    OO MM MMMM MM MM MMMM MM   AA  AA   NN NN NN DD   DD  SSSSS  ||
+                  ||  AAAAAAAA  PP      pp        CC       OO    OO MM  MM  MM MM  MM  MM  AAAAAAAA  NN  NNNN DD   DD      SS ||
+                  || AA      AA PP      pp         CCCCCCC  OOOOOO  MM      MM MM      MM AA      AA NN    NN DDDDDD  SSSSSS  ||
+                  ==============================================================================================================
+"""
+
+
+@BlackMarket.command(name = 'ld-ext=')
+async def ld_extntn(ctx : commands.Context, extension):
+	"""Load an extention to the bot"""
+	
+	id_str = str(ctx.author.id)
+	usr = ctx.author.name
+	
+	if id_str == MY_ID():
+		BlackMarket.load_extension(f'cog.{extension}')
+		print(f'{BlackMarket.user.name}    : loading extention\nStatus         : extension {extension} loaded')
+		await ctx.send(f"```css\nLoading Extension : cog.{extension} ```")
+	
+	else:
+		await ctx.send(Const.ERR.ERR_STR.format(usr))
+		print(Const.ERR.WARN_STR.format(usr, id_str, ))
+
+
+
+@BlackMarket.command(name = 'uld-ext=')
+async def ld_extntn(ctx : commands.Context, extension):
+	"""Unload an extention from the bot"""
+	
+	id_str = str(ctx.author.id)
+	usr = ctx.author.name
+	
+	if id_str == MY_ID():
+		BlackMarket.unload_extension(f'cog.{extension}')
+		print(f'{BlackMarket.user.name}    : unloading extention\nStatus         : extension {extension} unloaded')
+		await ctx.send(f"```css\nUnloading Extension : cog.{extension}```")
+	
+	else:
+		await ctx.send(Const.ERR.ERR_STR.format(usr))
+		print(Const.ERR.WARN_STR.format(usr, id_str, ))
+
+
+@BlackMarket.command(name = Const.CMD.EXT)
+async def shutdown(ctx : commands.Context, arg = "0"):
+	"""shuts down BlackMarket bot. format : -e [exit value]"""
+	
+	id = str(ctx.author.id)
+	usr = ctx.author.name
+	bot = BlackMarket.user.name
+	
+	if id == MY_ID():
+		await ctx.send(Const.Logout.INIT_SHUTDOWN.format(arg))
+		await ctx.bot.logout()
+		print(Const.Logout.END_SHUTDOWN.format(bot))
+	
+	else:
+		await ctx.send(Const.ERR.ERR_STR.format(usr))
+		print(Const.ERR.WARNING.format(usr, id, Const.CMD.EXT, arg))
+		
+		if arg.isnumeric():
+			sys.exit(arg)
+		
+		else:
+			sys.exit(2)
+
+
+@BlackMarket.command(name = Const.CMD.RST)
+async def reset_bot(ctx : commands.Context, arg = 0):
+	"""Resets BlackMarket"""
+	
+	id = str(ctx.author.id)
+	usr = ctx. author.name
+	bot = BlackMarket.user.name
+	
+	if id == MY_ID():
+		await ctx.send(Const.Logout.INIT_RESET.format(arg))
+		await ctx.bot.logout()
+		print(f'\nResetting {bot} ?,..,?')
+		
+		subprocess.call([sys.executable, Const.BOT_EXEC])
+	
+	else:
+		await ctx.send(ERR.ERR_STR.format(usr))
+		print(ERR.WARNING.format(usr, id, Const.CMD.EXT, arg))
+
+
+
+@BlackMarket.command(name = Const.CMD.PING)
+async def latency_test(ctx : commands.Context, arg = '1'):
+	"""test latency! format = -p [ammount of pings to send]"""
+	
+	id = ctx.author.id
+	usr = ctx.author.name
+	
+	if str(id) == MY_ID():
+		if arg.isnumeric():
+			for num in range(0, int(arg)):
+				await ctx.send(Const.LATENCY.format(BlackMarket))
+				print(f"Latency : {BlackMarket.latency}")
+	
+	else:
+		await ctx.send(Const.ERR.ERR_STR.format(usr))
+		print(Const.ERR.WARNING.format(usr, id, Const.CMD.PING, None))
+
+
+@BlackMarket.command(name = Const.CMD.DAD)
+async def daddy(ctx : commands.Context):
+	"""BlackMarket's creator"""
+	 
+	embed0 = discord.Embed(color = Const.COL.LIME)
+	
+	embed0.set_author(name = Const.AUTHOR, url = Const.URL.AUTHOR_GITHUB, icon_url = Const.URL.AUTHOR_ICON)
+	embed0.add_field(name = "Email", value = "`DvlshSftwr@protonmail.com`", inline = False)
+	embed0.add_field(name = "Discord", value = "`5P4{3 |_0RD |V|07|-|3RF|_|{K3R#7797`", inline = False)
+	embed0.set_footer(text = "Have a bug to report? Want to contribute? Find me here!\nJust...don't bug me for no reason, you degens e,..,e")
+	
+	await ctx.send(embed = embed0)
+
+
+# this function allows a user to send another server member a treat
+
+@BlackMarket.command(name = Const.CMD.FOOD)
+async def send_food(ctx : commands.Context, arg, member : discord.Member):
+	"""Send someone food"""
+	
+	await Store.open_inventory(member)
+	
+	usr = ctx.author
+	inv = await Store.get_inventory_data()
+	items = inv[f'{member.id}']["food"].keys()
+	index = 0
+	
+	for key in items:
+		if arg == key:
+			img_file = discord.File(f'img/{key}.png', filename = 'image.png')
+			
+			embed7 = discord.Embed(title = f'{usr.name} gave you a {key}!', color = Const.COL.GOLD)
+			
+			inv[f"{member.id}"]["food"][key] += 1
+			
+			embed7 = discord.Embed(title = f'{usr.name} gave you a {key.capitalize()}!', color = Const.COL.GOLD)
+			
+			embed7.add_field(name = f':{Const.EMJ.FOOD_EMOJI[index]}: = [{inv[f"{member.id}"]["food"][key]}]', value = f'```css\n Enjoy your munchies, you fatass! ^,..,^```', inline = False)
+			embed7.set_thumbnail(url = f'attachment://image.png')
+			break
+		
+		else:
+			index += 1
+		
+	
+	await Store.set_inventory_data(inv)
+	await ctx.send(f'<@{member.id}>')
+	await ctx.send(file = img_file, embed = embed7)
+
+
+# this function pulls up the user's inventory
+
+@BlackMarket.command(name = Const.CMD.INV)
+async def view_inventory(ctx : commands.Context):
+	"""View your inventory"""
+	
+	usr = ctx.author
+	
+	await Store.open_inventory(usr)
+	
+	inv = await Store.get_inventory_data()
+	name1 = "ability"
+	name2 = "talent"
+	ability_keys = list(inv[str(usr.id)][name1].keys())
+	talent_keys = list(inv[str(usr.id)][name2].keys())
+	cnt = 0
+	
+	embed2 = discord.Embed(title = "Your Item Inventory!", description = "```css\nTo use an item, type : --use= [item option]```", color = Const.COL.EGG_PLANT)
+	
+	
+	embed2.add_field(name = Const.Field.INVENTORY_NAMES[0], value = Const.Field.STORE_VALUES[0], inline = False)
+	
+	for number in range(1, 6):
+		embed2.add_field(name = Const.Field.INVENTORY_NAMES[number].format(inv[str(usr.id)][name1][ability_keys[cnt]]), value = Const.Field.STORE_VALUES[number], inline = True)
+		
+		cnt += 1
+	
+	embed2.add_field(name = Const.Field.INVENTORY_NAMES[7], value = Const.Field.STORE_VALUES[7], inline = False)
+	
+	cnt = 0
+	
+	for number in range(8, 21):
+		embed2.add_field(name = Const.Field.INVENTORY_NAMES[number].format(inv[str(usr.id)][name2][talent_keys[cnt]]), value = Const.Field.STORE_VALUES[number], inline = True)
+		
+		cnt += 1
+	
+	embed2.set_thumbnail(url = usr.avatar_url)
+	
+	await ctx.send(usr.mention)
+	await ctx.send(embed = embed2)
+
+
+
+"""
+                         ===============================================================================
+                         ||     AA     PPPPPP  pppppp     SSSSSSS TTTTTTTT    AA     RRRRRR  TTTTTTTT ||
+                         ||    AAAA    PP   PP pp   pp   SS          TT      AAAA    RR   RR    TT    ||
+                         ||   AA  AA   PPPPPP  pppppp     SSSSSS     TT     AA  AA   RRRRRR     TT    ||
+                         ||  AAAAAAAA  PP      pp               S    TT    AAAAAAAA  RR  RR     TT    ||
+                         || AA      AA PP      pp        SSSSSSS     TT   AA      AA RR   RR    TT    ||
+                         ===============================================================================
+"""
+
+
+def main_function():
+	
+	for file_name in os.listdir("./cog"):
+		if file_name.endswith(".py"):
+			BlackMarket.load_extension(f'cog.{file_name[:-3]}')
+	
+	BlackMarket.run(Const.TOKEN)
+	
+	return 0
+
+
+sys.exit(main_function())
---
+++ Constants.py
@@ -0,0 +1,214 @@
+
+
+class Constants:
+	BOT_EXEC = 'BlackMarket.py'
+	MAIN_DIR = '/home/anyenz/Documents/PythonFiles/BlackMarketBot'
+	TOKEN = ''
+	APP_DESC = 'BlackMarket is a custom economical management bot by Dvlsh::Sftwr'
+	PREFIX = '--'
+	GAME = 'with your mother\'s clit :stuck_out_tongue:'
+	WAL = 'wallet'
+	BNK ='bank'
+	LATENCY = '```py\nLatency : {0.latency}```'
+	AUTHOR = 'Fluffykins Allmighty'
+	
+	class EMJ:
+		FOOD_EMOJI = [
+			"cookie", 
+			"pizza", 
+			"hamburger", 
+			"fries", 
+			"pie", 
+			"cake", 
+			"cupcake", 
+			"moon_cake", 
+			"fish_cake", 
+			"poultry_leg", 
+			"cut_of_meat", 
+			"waffle", 
+			"pancakes", 
+			"ramen", 
+			"icecream", 
+			"taco", 
+			"burrito", 
+			"rice_ball", 
+			"sushi", 
+			"falafel", 
+			"sandwich", 
+			"pretzel", 
+			"ice_cream", 
+			"oden", 
+			"dango", 
+			"coffee", 
+			"tea", 
+			"lolipop", 
+			"candy", 
+			"chocolate_bar", 
+			"popcorn", 
+			"doughnut", 
+			"bubble_tea", 
+			"beverage_box", 
+			"sake", "beer", 
+			"wine_glass", 
+			"whisky", 
+			"cocktail", 
+			"curry"
+		]
+	
+	class Field:
+		STORE_NAMES = [
+			"`Temporary Ability Boosting Items`",
+			":syringe:\nTestosterone Shot\n${0}",
+			":muscle:\nMuscle Shirt \n${0}",
+			"<:drink_can:776774403404726282>\nCan Of Redbull\n${0}",
+			":eyeglasses:\nGlasses \n${0}",
+			"<:binoculars:776776363305861130>\nBinoculars\n${0}",
+			":athletic_shoe:\nFresh Kicks\n${0}",
+			"`Temporary Talent Boosting Items`'",
+			"<:tablet2:776789955833233419>\nE-Cyclopedia\n${0}",
+			"<:bluepotion:776510405153390623>\nSpecial Cologne\n${0}",
+			"<:camo:776783595170824213>\nCamouflage Suit\n${0}",
+			":levitate:\nSharp Suit\n${0}",
+			":notepad_spiral:\nInformant Tip\n${0}",
+			":notebook_with_decorative_cover:\nInstruction Manual\n${0}",
+			":school_satchel:\nSurvivalist's Kit\n${0}",
+			":medical_symbol:\nFirst Aid Kit\n${0}",
+			"<:voice:776788298902274048>\nVoice Modifier\n${0}",
+			":microscope:\nScience Kit\n${0}",
+			":satellite:\nJammer\n${0}",
+			"<:tablet:776787306816339968>\nDiagnostics Pad\n${0}",
+			":goggles:\nSpy Goggles\n${0}"
+			]
+		
+		INVENTORY_NAMES = [
+			"`Temporary Ability Boosting Items`",
+			":syringe:\nTestosterone Shot\n{0}/5",
+			":muscle:\nMuscle Shirt \n{0}/5",
+			"<:drink_can:776774403404726282>\nCan Of Redbull\n{0}/5",
+			":eyeglasses:\nGlasses \n{0}/5",
+			"<:binoculars:776776363305861130>\nBinoculars\n${0}/5",
+			":athletic_shoe:\nFresh Kicks\n{0}/5",
+			"`Temporary Talent Boosting Items`'",
+			"<:tablet2:776789955833233419>\nE-Cyclopedia\n${0}/15",
+			"<:bluepotion:776510405153390623>\nSpecial Cologne\n{0}/15",
+			"<:camo:776783595170824213>\nCamouflage Suit\n{0}/15",
+			":levitate:\nSharp Suit\n{0}/15",
+			":notepad_spiral:\nInformant Tip\n{0}/15",
+			":notebook_with_decorative_cover:\nInstruction Manual\n{0}/15",
+			":school_satchel:\nSurvivalist's Kit\n{0}/15",
+			":medical_symbol:\nFirst Aid Kit\n{0}/15",
+			"<:voice:776788298902274048>\nVoice Modifier\n{0}/15",
+			":microscope:\nScience Kit\n{0}/15",
+			":satellite:\nJammer\n{0}/15",
+			"<:tablet:776787306816339968>\nDiagnostics Pad\n{0}/15",
+			":goggles:\nSpy Goggles\n{0}"
+			]
+		
+		STORE_VALUES = [
+			"```css\nYou may have up to 5 of each```",
+			"A steroid shot to give you that extra OOMPH!\n\n+1d #MIGHT    \n\noption : steroids",
+			"Feel a little tougher with this bad-ass tank top!\n\n+1d #TOUGH    \n\noption : muscle-shirt",
+			"Need a little boost to get you through the day?\n\n+1d #WILL     \n\noption : redbull",
+			"Feel a little smarter with these snazzy specs!        \n\n\n+1d #INTEL      \n\noption : glasses",
+			"Need a better look at the milf next door? PFFT. Sure, ok. Whatever you say...\n\n+1d #PERC      \n\noption : binoculars",
+			"A fresh new set of sneakers! Better than...whatever you call those\n\n+1d #AGIL\n\noption : fresh-kicks",
+			"```css\nYou may have up to 15 of each```",
+			"A digital encyclopedia for all your 'on the go' informational needs\n\n\n\n\n\n\n+1 #ACAD\n\noption : e-cyclopedia",
+			"That stinky crap Mark and Cass wear that apperently has some kind of charming quality to others...\nShit just gives me a fucking headache, I swear... -,..,-\"\n\n+1d #CHARM\n\noption : cologne",
+			"My personal choice in attire for covert operations, very effective ^,..,^\n\n\n\n\n\n\n+1d #COVRT\n\noption : camouflage",
+			"Wanna seem a little more profesh? This snazzy suit'll get you there.\n\n\n\n+1d #PROF\n\noption : sharp-suit",
+			"Boy, do Ihave some juice for you...can't tell you who passed it on though. Client confidentiality, you know the drill.\n\n+1d #STRT\n\noption : info-tip",
+			"What, you don't know how to drive that thing?! All good, homie, I gotchu. Peep this.\n\n\n+1d #VEHIC\noption : \ninstruction-manual",
+			"Roughing it? I've go t just what you need, all the tools you need to make it through...if you're not a total dumbass, that is...\n\n+1d #SURV\n\noption : survival-kit",
+			"Always good to have some med supplies on you, fights can get rough!\n\n\n\n+1d #MEDIC\n\noption : med-kit",
+			"Sound a little more 'in command' with this little dohicky\n\n\n\n\n+1 #CMND\n\noption : voice-mod",
+			"A little prtable kit for all your scientific needs...if you're smart enough to use it, that is\n\n+1d #SCINC\n\noption : science-kit",
+			"A signal jammer. Built it myself. It...should work...I think. ^,..,^\"\n\n\n+1d #TECH\n\noption : jammer",
+			"A diagnostics pad for system analytics.\n\n\n\n\n+1d #TECH\noption : \ndiagnotsic-pad",
+			"Need to do some PI work? These suckers magnify, hide your eyes, and have optical display tech built in. Built em myself.\n\n+1d #INVS\n\noption : goggles"
+			]
+		
+		
+	class F:
+		JSON_FILE_1 = 'cog/Bank.json'
+		JSON_FILE_2 = 'cog/Store.json'
+		JSON_FILE_3 = 'cog/Inventory.json'
+	
+	
+	class Login:
+		LOGGED_IN = '\nClient         : Logged in as\nUSER           : {0}\nID             : {1} '
+					 
+		LOADING = 'Loading        : {0}'
+		OPEN = '{0}    : open for business! ^,..,^'
+	
+	
+	class Logout:
+		INIT_SHUTDOWN = '```CSS\nShutdown process initiated!\nLogging out and shutting down now...\nexit : {0}```'
+		END_SHUTDOWN = '\n{0} has meen shut down\nIt\'s been fun, have a nice day ^,..,^'
+		INIT_RESET = '```CSS\nReset process initiated!\nLogging out and shutting down now...\nexit : {0}```'
+	
+	
+	class ERR:
+		ERR_STR = "```CSS\n{0}yooooou're NOT my FUCKING father! E,..,E\nFuck right off with that shit!\n I'm telling on you!```"
+		WARN_STR = 'User : {0}\nID : {1}\n This user just tried to activate : {2}\nWith value : {3}\nOperation aborted, warning sent to dad'
+	
+	
+	class URL:
+		DEF_WALLET_IMG = 'https://image.freepik.com/free-photo/dirty-hands-homeless-poor-man-with-empty-wallet-modern-capitalism-society_140289-3.jpg'
+		WALLET_IMG_1 = 'https://dayair.org/wp-content/uploads/2017/04/money-wallet.jpg'
+		WALLET_IMG_2 = 'http://www.todayifoundout.com/wp-content/uploads/2017/10/wallet-full-of-money.png'
+		WALLET_IMG_3 = 'https://www.star991.com/images/blog/old-wallet-with-useless-papers-picture-id139671232.jpg'
+		WALLET_IMG_4 = 'https://www.catster.com/wp-content/uploads/2015/06/cash-cat-1.jpg'
+		WALLET_IMG_5 = 'https://cdn.ebaumsworld.com/mediaFiles/picture/910655/80810319.jpg'
+		WALLET_IMG_6 = 'http://brucemctague.com/wp-content/uploads/2014/08/money-stack-of.jpg'
+		AUTHOR_GITHUB = 'https://github.com/DvlshSftwr'
+		AUTHOR_ICON = 'https://avatars3.githubusercontent.com/u/71295247?s=400&u=591ad97be84b88ab9d59d47653d0dd6af6c60561&v=4'
+		E = 'https://i.imgur.com/x3PSV9Y.jpg'
+		F = 'https://img.pixers.pics/pho_wat(s3:700/FO/38/21/09/85/700_FO38210985_76f532408a9bb431aed242a945e56cc9.jpg,700,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,480,650,jpg)/wall-murals-fire-alphabet-letter-f.jpg.jpg'
+		COOKIE = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Femojis.wiki%2Femoji-pics%2Fwhatsapp%2Fcookie-whatsapp.png&f=1&nofb=1'
+		BURGER = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Femojis.wiki%2Femoji-pics%2Ffacebook%2Fhamburger-facebook.png&f=1&nofb=1'
+		FRIES = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Femojis.wiki%2Femoji-pics%2Ffacebook%2Ffrench-fries-facebook.png&f=1&nofb=1'
+		TWINKIE_HOUSES = [
+			'https://i.imgur.com/lkkAuOB.jpg',
+			'https://i0.kym-cdn.com/photos/images/original/000/022/614/3JJN4VJA7USJQLD5WKCLFJBLC6LJFG5P.png'
+			'https://i0.kym-cdn.com/photos/images/original/000/896/675/78e.jpg',
+			'https://external-preview.redd.it/tj_19XlsUL8TUe2RFyWumq7dznJ3ZwNcaFwO0B84mJg.jpg?auto=webp&s=d786b8b0839e7b965bad3761e6d434f11bc9474c',
+			'https://t04.deviantart.net/LbJTfltaaqNm5Hp0GuJZPuNXi1g=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre11/dbf4/th/pre/f/2009/274/4/2/twinkie_house_by_nekoninja12.png',
+			'https://64.media.tumblr.com/196fda28bb30dde26ebbddb0c089ecce/tumblr_p0luyz0CQr1vheqneo1_500.png' 
+		]
+	
+	class COL:
+		LIME = 0x40FF00
+		VERMILION = 0xFF4D00
+		EGG_PLANT = 0x390042
+		GOLD = 0xFFD700
+		FUCHA = 0xFF0040
+	
+	class CMD:
+		BAL = 'atm'
+		EXT = 'e'
+		PING = 'ping='
+		RST = 'r'
+		HLP = 'h'
+		SEND = 'send='
+		SHOP = 'shop'
+		INV = 'inventory'
+		CHCK = 'check='
+		USE = 'use='
+		BUY = 'buy='
+		TRAN = 'atm='
+		USE = 'use='
+		INV = 'inventory'
+		RNG = 'rng='
+		DAD = 'papi'
+		PROS = 'hoe'
+		WORK = 'work'
+		E = 'E'
+		F = 'F'
+		FOOD = 'food='
+		TWH = 'twinkie-house='
+		MTL_BENDING = 'gat-bending'
+def MY_ID():
+	return '415262753230422036'
+
+Const = Constants()
